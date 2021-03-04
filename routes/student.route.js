f0229b815f6b6fbc647db89305602df9d019a029
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-03-04 13:37:55 -0500

    added a student route with CRUD methods for add student, delete student, update student, get all students, and get a student

---
+++ routes/student.route.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const studentRoute = express.Router();
+
+//Student model
+let Student = require('..model/Student');
+
+//Add Student
+studentRoute.route('/add-student').post((req, resp, next) =>{
+    Student.create(req.body, (error, data) =>{
+        if (error){
+            return next(error)
+        } else {
+            resp.json(data)
+        }
+    })
+});
+
+//Get all students
+studentRoute.route('/').get((req, res) => {
+    Student.find((error, data) => {
+        if(error){
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+//Get one student
+studentRoute.route('/read-student/:id').get((req, res) => {
+    Student.findById(req.params.id, (error, data) => {
+        if(error){
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+//update student
+studentRoute.route('/update-student/:id').put((req, res, next) =>{
+    Student.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data)=>{
+        if (error) {
+            return next(error);
+            console.log(error)
+        } else {
+            res.json(data)
+            console.log('Student successfully updated!');
+        }
+    })
+})
+
+//Delete Student
+studentRoute.route('/delete-student/:id').delete((req, res, next)=>{
+    if (error){
+        return next(error);
+    } else {
+        res.status(200).json({
+            msg: data
+        })
+    }
+})
+
+module.exports = studentRoute;
